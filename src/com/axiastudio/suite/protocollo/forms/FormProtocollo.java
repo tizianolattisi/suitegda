a6f87d291aa460fd4dec138a40b0b10cd9920e6b
Tiziano Lattisi <tiziano@axiastudio.it>
2012-06-07 17:20:16 +0200

    Aggiustamenti alla form protocollo per il test di Alfresco e PyPaPiComboBox

--- src/com/axiastudio/suite/protocollo/forms/FormProtocollo.java
+++ src/com/axiastudio/suite/protocollo/forms/FormProtocollo.java
@@ -7,9 +7,12 @@ package com.axiastudio.suite.protocollo.forms;
 import com.axiastudio.pypapi.ui.Form;
 import com.axiastudio.pypapi.ui.widgets.PyPaPiTableView;
 import com.axiastudio.pypapi.ui.widgets.PyPaPiToolBar;
+import com.axiastudio.suite.alfresco.AlfrescoHelper;
+import com.axiastudio.suite.alfresco.AlfrescoObject;
 import com.axiastudio.suite.protocollo.entities.Protocollo;
-import com.trolltech.qt.gui.QLabel;
-import com.trolltech.qt.gui.QPixmap;
+import com.trolltech.qt.core.QUrl;
+import com.trolltech.qt.gui.*;
+import java.util.Calendar;
 
 class ProtocolloMenuBar extends PyPaPiToolBar {
     public ProtocolloMenuBar(String title, Form parent){
@@ -20,6 +23,9 @@ class ProtocolloMenuBar extends PyPaPiToolBar {
         this.insertButton("convalidaProtocollo", "Convalida protocollo",
                 "classpath:com/axiastudio/suite/resources/lock_mail.png",
                 "Convalida della registrazione del protocollo", parent);
+        this.insertButton("spazioAlfresco", "Spazio Alfresco",
+                "classpath:com/axiastudio/suite/resources/alfresco.png",
+                "Apri spazio Alfresco nel browser", parent);
     }
 }
 
@@ -29,6 +35,12 @@ class ProtocolloMenuBar extends PyPaPiToolBar {
  */
 public class FormProtocollo extends Form {
     private final ProtocolloMenuBar protocolloMenuBar;
+    private QTabWidget tabWidget;
+    private AlfrescoHelper alfrescoHelper;
+    private final String ALFRESCOCMIS="http://127.0.0.1:8080/alfresco/service/cmis";
+    private final String ALFRESCOHOST="127.0.0.1";
+    private final Integer ALFRESCOPORT=8080;
+    private final String ALFRESCOURL="http://127.0.0.1:8080/share/page/dologin?username=admin&password=admin&success=/share/page/site/protocollo/documentlibrary#filter=path%%7C/%s&page=1";
     
     public FormProtocollo(String uiFile, Class entityClass, String title){
         super(uiFile, entityClass, title);
@@ -40,6 +52,10 @@ public class FormProtocollo extends Form {
         labelConvalidaProtocollo.setPixmap(new QPixmap("classpath:com/axiastudio/suite/resources/lock_mail.png"));
         QLabel labelConvalidaAttribuzioni = (QLabel) this.findChild(QLabel.class, "labelConvalidaAttribuzioni");
         labelConvalidaAttribuzioni.setPixmap(new QPixmap("classpath:com/axiastudio/suite/resources/lock_group.png"));
+        
+        this.tabWidget = (QTabWidget) this.findChild(QTabWidget.class, "tabWidget");
+        this.tabWidget.currentChanged.connect(this, "currentTabChanged(int)");
+        this.alfrescoHelper = new AlfrescoHelper("admin", "admin", this.ALFRESCOCMIS);
     }
     
     private void convalidaAttribuzioni() {
@@ -54,6 +70,11 @@ public class FormProtocollo extends Form {
         protocollo.setConvalidaProtocollo(Boolean.TRUE);
         this.getContext().getDirty();
     }
+    
+    private void spazioAlfresco(){
+        String path = String.format(this.ALFRESCOURL, this.getAlfrescoPath());
+        QDesktopServices.openUrl(new QUrl(path));
+    }
 
     @Override
     protected void indexChanged(int row) {
@@ -67,4 +88,27 @@ public class FormProtocollo extends Form {
         this.centralWidget().setEnabled(!convProtocollo);
     }
 
+    private void currentTabChanged(int tab) {
+        String tabText = this.tabWidget.tabText(tab);
+        QListWidget qlw = (QListWidget) this.findChild(QListWidget.class, "listWidgetAlfresco");
+        qlw.clear();
+        if( "Documenti".equals(tabText) ){
+            String path = "/Siti/protocollo/documentLibrary"+this.getAlfrescoPath();
+            for(AlfrescoObject object: this.alfrescoHelper.childrenNames(path)){
+                qlw.addItem(object.getName());
+            }
+        }
+    }
+    
+    private String getAlfrescoPath(){
+        Protocollo protocollo = (Protocollo) this.getContext().getCurrentEntity();
+        Calendar c = Calendar.getInstance();
+        c.setTime(protocollo.getDataprotocollo());
+        String path = "/"+c.get(Calendar.YEAR)+
+                String.format("/%02d/", c.get(Calendar.MONTH)+1)+
+                String.format("%02d/", c.get(Calendar.DAY_OF_MONTH))+
+                protocollo.getIddocumento();
+        return path;
+    }
+        
 }
