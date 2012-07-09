349d5e6c26120fe87f252acf12413b98747a28d8
Tiziano Lattisi <tiziano@axiastudio.it>
2012-07-09 16:29:13 +0200

    Soggetto, cambio tab a seconda del tipo.

---
+++ src/com/axiastudio/suite/anagrafiche/forms/FormSoggetto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.axiastudio.suite.anagrafiche.forms;
+
+import com.axiastudio.pypapi.ui.Form;
+import com.trolltech.qt.gui.QComboBox;
+import com.trolltech.qt.gui.QTabWidget;
+
+/**
+ *
+ * @author Tiziano Lattisi <tiziano at axiastudio.it>
+ */
+public class FormSoggetto extends Form {
+    
+    public FormSoggetto(String uiFile, Class entityClass, String title){
+        super(uiFile, entityClass, title);
+        QComboBox tipologiaSoggetto = (QComboBox) this.findChild(QComboBox.class, "comboBoxTipologiaSoggetto");
+        tipologiaSoggetto.currentIndexChanged.connect(this, "refresh(Integer)");
+    }
+    
+    private void refresh(Integer idx){
+        QTabWidget tab = (QTabWidget) this.findChild(QTabWidget.class, "tabWidgetHeader");
+        tab.setCurrentIndex(idx);
+    }
+    
+}
