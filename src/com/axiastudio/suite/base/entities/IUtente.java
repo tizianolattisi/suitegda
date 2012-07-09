54861b9228c71c97145ee1e7d01e74ebc60382c2
Tiziano Lattisi <tiziano@axiastudio.it>
2012-07-09 16:28:19 +0200

    Interfaccia IUtente (per registrazione utility)

---
+++ src/com/axiastudio/suite/base/entities/IUtente.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.axiastudio.suite.base.entities;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Tiziano Lattisi <tiziano at axiastudio.it>
+ */
+public interface IUtente extends Serializable {
+    
+}
