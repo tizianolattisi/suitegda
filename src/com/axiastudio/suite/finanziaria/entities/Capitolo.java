15416d43b35cd6aa3e82ff1c15a2ba50cee1d94b
Tiziano Lattisi <tiziano@axiastudio.it>
2013-03-24 10:39:14 +0100

    Capitolo

---
+++ src/com/axiastudio/suite/finanziaria/entities/Capitolo.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.axiastudio.suite.finanziaria.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author AXIA Studio (http://www.axiastudio.com)
+ */
+@Entity
+@Table(schema="finanziaria")
+public class Capitolo implements Serializable {
+
+    @Id
+    private Long id;
+    @Column(name="descrizione")
+    private String descrizione;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Capitolo)) {
+            return false;
+        }
+        Capitolo other = (Capitolo) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.axiastudio.suite.deliberedetermine.entities.Capitolo[ id=" + id + " ]";
+    }
+    
+}
