ee228cf10a8b657895104d1cf2e522ab0ba7e00b
Tiziano Lattisi <tiziano@axiastudio.it>
2013-05-12 21:31:07 +0200

    Correzione generazione id pratica (9 caratteri).

--- src/com/axiastudio/suite/pratiche/PraticaCallbacks.java
+++ src/com/axiastudio/suite/pratiche/PraticaCallbacks.java
@@ -100,9 +100,9 @@ public class PraticaCallbacks {
             if( max != null ){
                 Integer i = Integer.parseInt(max.getIdpratica().substring(4));
                 i++;
-                newIdpratica = year+String.format("%08d", i);
+                newIdpratica = year+String.format("%05d", i);
             } else {
-                newIdpratica = year+"00000001";
+                newIdpratica = year+"00001";
             }
             pratica.setIdpratica(newIdpratica);
             
