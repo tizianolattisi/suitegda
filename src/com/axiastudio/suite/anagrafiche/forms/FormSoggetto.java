a11637ffb116fe67cdf431931f68f6d5d002032b
Tiziano Lattisi <tiziano@axiastudio.it>
2013-05-14 15:51:21 +0200

    Tre predicate provider per filtrare i gruppi sulla base della tipologia del soggetto.

--- src/com/axiastudio/suite/anagrafiche/forms/FormSoggetto.java
+++ src/com/axiastudio/suite/anagrafiche/forms/FormSoggetto.java
@@ -16,10 +16,21 @@
  */
 package com.axiastudio.suite.anagrafiche.forms;
 
+import com.axiastudio.pypapi.Register;
+import com.axiastudio.pypapi.db.ICriteriaFactory;
 import com.axiastudio.pypapi.ui.Window;
 import com.axiastudio.suite.SuiteUiUtil;
+import com.axiastudio.suite.anagrafiche.entities.Gruppo;
+import com.axiastudio.suite.anagrafiche.entities.Gruppo_;
+import com.axiastudio.suite.anagrafiche.entities.Soggetto;
 import com.trolltech.qt.gui.QComboBox;
 import com.trolltech.qt.gui.QTabWidget;
+import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 /**
  *
@@ -33,17 +44,45 @@ public class FormSoggetto extends Window {
         tipoSoggetto.currentIndexChanged.connect(this, "refresh(Integer)");
         this.refresh(tipoSoggetto.currentIndex());
     }
-    
+
+    /*
+     * Abilitazione del tab corretto e filtro sui gruppi
+     */
     private void refresh(Integer idx){
+        String[] gruppi = {"P", "A", "E"};
         QTabWidget tab = (QTabWidget) this.findChild(QTabWidget.class, "tabWidgetHeader");
         tab.setCurrentIndex(idx);
         for( int i=0; i<3; i++ ){
-                tab.setTabEnabled(i, i==idx);
+            tab.setTabEnabled(i, i==idx);
+        }
+        try {
+            Method provider = FormSoggetto.class.getMethod("gruppo"+gruppi[idx]+"PredicateProvider", CriteriaBuilder.class, Root.class);
+            Register.registerUtility(provider, ICriteriaFactory.class, Gruppo.class.getName());
+        } catch (NoSuchMethodException ex) {
+            Logger.getLogger(FormSoggetto.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(FormSoggetto.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
     private void information() {
         SuiteUiUtil.showInfo(this);
+    }
+    
+    /*
+     * Un predicato per filtrare i gruppi sulla base della tipologia di soggetto
+     */
+    public static Predicate gruppoAPredicateProvider(CriteriaBuilder cb, Root from) {
+        Predicate predicate = cb.equal(from.get(Gruppo_.azienda), Boolean.TRUE);
+        return predicate;
+    }
+    public static Predicate gruppoPPredicateProvider(CriteriaBuilder cb, Root from) {
+        Predicate predicate = cb.equal(from.get(Gruppo_.persona), Boolean.TRUE);
+        return predicate;
+    }
+    public static Predicate gruppoEPredicateProvider(CriteriaBuilder cb, Root from) {
+        Predicate predicate = cb.equal(from.get(Gruppo_.ente), Boolean.TRUE);
+        return predicate;
     }
     
 }
