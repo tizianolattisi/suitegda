b97eb03d18ade4c69723ec71f5f635da7ef3c328
Tiziano Lattisi <tiziano@axiastudio.it>
2013-12-03 08:58:33 +0100

    Override di equals per permettere il funzionamento del select del PyPaPiComboBox (lo store iniziale è su un altro em…).

--- src/com/axiastudio/suite/procedimenti/entities/FaseProcedimento.java
+++ src/com/axiastudio/suite/procedimenti/entities/FaseProcedimento.java
@@ -160,4 +160,19 @@ public class FaseProcedimento implements Serializable {
     public String toString() {
         return getFase().getDescrizione();
     }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof FaseProcedimento)) {
+            return false;
+        }
+        FaseProcedimento other = (FaseProcedimento) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+
+    }
+
 }
