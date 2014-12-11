f0abf1eb46c2c4cc32fc01b8700f10b2e53f3e49
Tiziano Lattisi <tiziano@axiastudio.it>
2014-12-11 09:43:57 +0100

    La ricerca esegue uno spillo su "#", e verifica ogni stringa lunga 12.

--- src/com/axiastudio/suite/scanndo/ScanNDo.java
+++ src/com/axiastudio/suite/scanndo/ScanNDo.java
@@ -68,32 +68,44 @@ public class ScanNDo extends QDialog {
     }
 
     private void doSomething(){
-        String text = scansione.text();
-
         Database db = (Database) Register.queryUtility(IDatabase.class);
-        EntityManager em = db.getEntityManagerFactory().createEntityManager();
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Protocollo> cq = cb.createQuery(Protocollo.class);
-        Root from = cq.from(Protocollo.class);
-        Predicate predicate = cb.equal(from.get("iddocumento"), text);
-        cq.select(from);
-        cq.where(predicate);
-        TypedQuery<Protocollo> tq = em.createQuery(cq);
-        if ( tq.getResultList().size() == 0 ) {
-            QMessageBox.warning(this, "Attenzione", "Protocollo non trovato.");
-            return;
+        String text = scansione.text();
+        String[] parts = text.split("#");
+        for( String test: parts ){
+            if( test.length() == 12 ){
+                EntityManager em = db.getEntityManagerFactory().createEntityManager();
+                CriteriaBuilder cb = em.getCriteriaBuilder();
+                CriteriaQuery<Protocollo> cq = cb.createQuery(Protocollo.class);
+                Root from = cq.from(Protocollo.class);
+                Predicate predicate = cb.equal(from.get("iddocumento"), test);
+                cq.select(from);
+                cq.where(predicate);
+                TypedQuery<Protocollo> tq = em.createQuery(cq);
+                if ( tq.getResultList().size() == 0 ) {
+                    QMessageBox.warning(this, "Attenzione", "Protocollo non trovato.");
+                    em.close();
+                    return;
+                }
+                Protocollo protocollo = tq.getSingleResult();
+                if( !protocollo.getSpedito() ) {
+                    protocollo.setSpedito(true);
+                    // utilizzo direttamente l'entity manager così non passo per le callback
+                    em.getTransaction().begin();
+                    em.persist(protocollo);
+                    em.getTransaction().commit();
+                    em.close();
+                    beep.play();
+                    return;
+                } else {
+                    QMessageBox.warning(this, "Attenzione", "Protocollo già spedito.");
+                    em.close();
+                    return;
+                }
+
+            }
         }
-        Protocollo protocollo = tq.getSingleResult();
-        if( !protocollo.getSpedito() ) {
-            protocollo.setSpedito(true);
-            // utilizzo direttamente l'entity manager così non passo per le callback
-            em.getTransaction().begin();
-            em.persist(protocollo);
-            em.getTransaction().commit();
-            beep.play();
-        } else {
-            QMessageBox.warning(this, "Attenzione", "Protocollo già spedito.");
-        }
+        QMessageBox.warning(this, "Attenzione", "Identificativo protocollo non trovato.");
+
     }
 
 }
