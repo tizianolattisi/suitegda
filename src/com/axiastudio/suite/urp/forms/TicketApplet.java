86d8188b163ffe813301f3ab7c67a54a55dc1f76
Tiziano Lattisi <tiziano@axiastudio.it>
2015-02-25 17:58:54 +0100

    L'ultimo sportello che chiude passa gli ultimi servizi agli altri.

--- src/com/axiastudio/suite/urp/forms/TicketApplet.java
+++ src/com/axiastudio/suite/urp/forms/TicketApplet.java
@@ -4,6 +4,7 @@ import com.axiastudio.pypapi.Register;
 import com.axiastudio.pypapi.db.Database;
 import com.axiastudio.pypapi.db.IDatabase;
 import com.axiastudio.pypapi.db.Store;
+import com.axiastudio.pypapi.ui.Util;
 import com.axiastudio.suite.base.entities.IUtente;
 import com.axiastudio.suite.base.entities.Utente;
 import com.axiastudio.suite.urp.entities.ServizioAlCittadino;
@@ -14,11 +15,9 @@ import com.trolltech.qt.core.Qt;
 import com.trolltech.qt.gui.*;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.*;
 
@@ -30,7 +29,7 @@ import java.util.*;
 
 public class TicketApplet extends QDialog {
 
-    private final Sportello sportello;
+    private Sportello sportello;
     private QLabel labelSportello;
     private QLabel labelChiamato;
     private QToolButton buttonConfig;
@@ -46,6 +45,9 @@ public class TicketApplet extends QDialog {
 
         // setup dello sportello
         this.sportello = sportello;
+        sportello.setAttivo(Boolean.TRUE);
+        Database db = (Database) Register.queryUtility(IDatabase.class);
+        db.createController(Sportello.class).commit(this.sportello);
 
         // geometry
         setGeometry(60, 60, 200, 26);
@@ -95,25 +97,17 @@ public class TicketApplet extends QDialog {
 
     }
 
+    private void refreshSportello(){
+        Database db = (Database) Register.queryUtility(IDatabase.class);
+        sportello = (Sportello) db.createController(Sportello.class).get(sportello.getId());
+    }
+
     private void configurazione(){
 
-        Database db = (Database) Register.queryUtility(IDatabase.class);
+        refreshSportello();
 
         // numero di sportelli agganciati ad ogni servizio
-        Store scscTutti = db.createController(ServizioAlCittadinoSportello.class).createFullStore();
-        Map<ServizioAlCittadino, Integer> ns = new HashMap<>();
-        for( Object obj: scscTutti ){
-
-            ServizioAlCittadinoSportello scs = (ServizioAlCittadinoSportello) obj;
-            if (!ns.keySet().contains(scs.getServizioalcittadino())) {
-                ns.put(scs.getServizioalcittadino(), 0);
-            }
-            if( scs.getAttivo() ){
-                ns.put(scs.getServizioalcittadino(), ns.get(scs.getServizioalcittadino()) + 1);
-            }
-        }
-
-        System.out.println(ns);
+        Map<ServizioAlCittadino, Integer> ns = getSportelliPerServizio();
 
         QDialog dialog = new QDialog(this);
         QVBoxLayout layout = new QVBoxLayout(dialog);
@@ -127,7 +121,7 @@ public class TicketApplet extends QDialog {
             checkBox.setText(servizio.getDescrizione());
             if( scs.getAttivo() ) {
                 checkBox.setCheckState(Qt.CheckState.Checked);
-                checkBox.setEnabled(ns.get(servizio)>1);
+                checkBox.setEnabled(ns.get(servizio) > 1);
             } else {
                 checkBox.setCheckState(Qt.CheckState.Unchecked);
             }
@@ -145,9 +139,40 @@ public class TicketApplet extends QDialog {
             for( int i=0; i<scsc.size(); i++ ){
                 scsc.get(i).setAttivo(Qt.CheckState.Checked.equals(checkBoxes.get(i).checkState()));
             }
+            Database db = (Database) Register.queryUtility(IDatabase.class);
             db.createController(Sportello.class).commit(sportello);
 
         }
+    }
+
+    private void passaServizio(ServizioAlCittadino servizio){
+        Database db = (Database) Register.queryUtility(IDatabase.class);
+        Store scscTutti = db.createController(ServizioAlCittadinoSportello.class).createFullStore();
+        for( Object obj: scscTutti ){
+            ServizioAlCittadinoSportello scs = (ServizioAlCittadinoSportello) obj;
+            if( !scs.getSportello().equals(sportello) && scs.getSportello().getAttivo() ){
+                if( scs.getServizioalcittadino().equals(servizio) && !scs.getAttivo() ){
+                    scs.setAttivo(Boolean.TRUE);
+                    db.createController(ServizioAlCittadinoSportello.class).commit(scs);
+                }
+            }
+        }
+    }
+
+    private Map<ServizioAlCittadino, Integer> getSportelliPerServizio() {
+        Store scscTutti = ((Database) Register.queryUtility(IDatabase.class)).createController(ServizioAlCittadinoSportello.class).createFullStore();
+        Map<ServizioAlCittadino, Integer> ns = new HashMap<>();
+        for( Object obj: scscTutti ){
+
+            ServizioAlCittadinoSportello scs = (ServizioAlCittadinoSportello) obj;
+            if (!ns.keySet().contains(scs.getServizioalcittadino())) {
+                ns.put(scs.getServizioalcittadino(), 0);
+            }
+            if( scs.getAttivo() ){
+                ns.put(scs.getServizioalcittadino(), ns.get(scs.getServizioalcittadino()) + 1);
+            }
+        }
+        return ns;
     }
 
     private void informazioni(){
@@ -291,6 +316,7 @@ public class TicketApplet extends QDialog {
      *  Chiama il ticket successivo:
      */
     private Ticket chiamaSuccessivo(){
+        refreshSportello();
         chiudiTicketAttuale();
         Database db = (Database) Register.queryUtility(IDatabase.class);
         EntityManager em = db.getEntityManagerFactory().createEntityManager();
@@ -336,4 +362,26 @@ public class TicketApplet extends QDialog {
         return calendar.getTime();
     }
 
+    @Override
+    protected void closeEvent(QCloseEvent event) {
+        String msg = "Desideri chiudere lo sportello? I servizi non più gestiti verranno trasferiti ai rimanenti sportelli aperti.";
+        Boolean res = Util.questionBox(this, "Chiusura sportello", msg);
+        if( !res ){
+            event.ignore();
+            return;
+        }
+        Map<ServizioAlCittadino, Integer> ns = getSportelliPerServizio();
+        for( ServizioAlCittadinoSportello scs: sportello.getServizioalcittadinosportelloCollection() ){
+            if( scs.getAttivo() ){
+                if( ns.get(scs.getServizioalcittadino())<2 ){
+                    passaServizio(scs.getServizioalcittadino());
+                }
+                scs.setAttivo(Boolean.FALSE);
+            }
+        }
+        sportello.setAttivo(Boolean.FALSE);
+        Database db = (Database) Register.queryUtility(IDatabase.class);
+        db.createController(Sportello.class).commit(sportello);
+        super.closeEvent(event);
+    }
 }
